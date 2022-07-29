fec02f624fb7e1ea18d88ca6cdd0c22a9c2404c6
Jean <x22me22x@outlook.com>
2022-07-29 13:35:33 +0300

    add some shop items and poop

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,13 +1,17 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from f.stuff.shopitems import shopitems
 
 class Economy(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.currency = "coins"
+		self.shopitems = shopitems
 	
 	group = app_commands.Group(name="economy", description=f"Economy commands: earn coins, spend coins, and more")
+
+	# some of the economy commands will be in levelling.py
 
 async def setup(bot):
     await bot.add_cog(Economy(bot))
---
+++ f/stuff/shopitems.py
@@ -0,0 +1,42 @@
+shopitems = {
+	# bug killers
+	"Bug Spray": {
+		"name": "Bug Spray",
+		"description": "Spray pests (and enemies) away with this bug spray.",
+		"price": 7, # you can buy it in Daiso!
+		"uses": 1
+	},
+	"Slippers": {
+		"name": "Slippers",
+		"description": "The classic bug-slapper.",
+		"price": 15, # also from Daiso!
+		"uses": 25 # more will become pretty gross
+	},
+	"Trainers": {
+		"name": "Trainers",
+		"description": "The bug-stomper that also lets you run away if it's too scary.",
+		"price": 30,
+		"uses": 60
+	},
+	"Flypaper": {
+		"name": "Flypaper",
+		"description": "Deadly sticky paper for flies.",
+		"price": 14,
+		"uses": -1 # lasts the whole month
+	},
+
+	# fertilizers
+	"Synthetic fertilizer": {
+		"name": "Synthetic fertilizer",
+		"description": "Manmade stuff to make plants grow healthier.",
+		"price": 50,
+		"uses": -1
+	},
+	"Organic fertilizer": {
+		"name": "Organice fertilizer",
+		"description": "Natural stuff to make plants grow healthier. And by natural, we mean it's poop.",
+		"price": 75,
+		"uses": -3 # lasts three months
+	}
+
+}

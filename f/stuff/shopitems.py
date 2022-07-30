c69eca363c1a09b918294c9e24b529699e6dd46d
Jean <x22me22x@outlook.com>
2022-07-30 20:22:49 +0300

    add new stuff to the shop

--- f/stuff/shopitems.py
+++ f/stuff/shopitems.py
@@ -2,25 +2,25 @@ shopitems = {
 	# bug killers
 	"Bug Spray": {
 		"name": "Bug Spray",
-		"description": "Spray pests (and enemies) away with this bug spray.",
+		"description": "Spray pests (and enemies) away with this bug spray. Kills bugs when hunting.",
 		"price": 7, # you can buy it in Daiso!
 		"uses": 1
 	},
 	"Slippers": {
 		"name": "Slippers",
-		"description": "The classic bug-slapper.",
+		"description": "The classic bug-slapper. Kills bugs when hunting.",
 		"price": 15, # also from Daiso!
 		"uses": 25 # more will become pretty gross
 	},
 	"Trainers": {
 		"name": "Trainers",
-		"description": "The bug-stomper that also lets you run away if it's too scary.",
+		"description": "The bug-stomper that also lets you run away if it's too scary. Kills bugs when hunting.",
 		"price": 30,
 		"uses": 60
 	},
 	"Flypaper": {
 		"name": "Flypaper",
-		"description": "Deadly sticky paper for flies.",
+		"description": "Deadly sticky paper for flies. Kills bugs when levelling up.",
 		"price": 14,
 		"uses": -1 # lasts the whole month
 	},
@@ -28,15 +28,23 @@ shopitems = {
 	# fertilizers
 	"Synthetic fertilizer": {
 		"name": "Synthetic fertilizer",
-		"description": "Manmade stuff to make plants grow healthier.",
+		"description": "Manmade stuff to make plants grow healthier. Gives extra xp when chatting.",
 		"price": 50,
 		"uses": -1
 	},
 	"Organic fertilizer": {
 		"name": "Organice fertilizer",
-		"description": "Natural stuff to make plants grow healthier. And by natural, we mean it's poop.",
+		"description": "Natural stuff to make plants grow healthier. And by natural, we mean it's poop. Gives extra xp when chatting.",
 		"price": 75,
 		"uses": -3 # lasts three months
+	},
+
+	# long-lasting products
+	"Python": {
+		"name": "Python",
+		"description": "A mostly-friendly python to live in your tree. Kills extra bugs when hunting, and occasionally gives xp boosts when chatting.",
+		"price": 100,
+		"uses": -12 # six month lifespan wow
 	}
 
 }

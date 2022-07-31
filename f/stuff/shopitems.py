363f5dce3299d71e45c5e9786342a27c3a3b14ff
Jean <x22me22x@outlook.com>
2022-07-31 10:30:00 +0300

    quality > quantity

--- f/stuff/shopitems.py
+++ f/stuff/shopitems.py
@@ -4,25 +4,25 @@ shopitems = {
 		"name": "Bug Spray",
 		"description": "Spray pests (and enemies) away with this bug spray. Kills bugs when hunting.",
 		"price": 7, # you can buy it in Daiso!
-		"uses": 1
+		"quality": 1
 	},
 	"Slippers": {
 		"name": "Slippers",
 		"description": "The classic bug-slapper. Kills bugs when hunting.",
 		"price": 15, # also from Daiso!
-		"uses": 25 # more will become pretty gross
+		"quality": 5
 	},
 	"Trainers": {
 		"name": "Trainers",
 		"description": "The bug-stomper that also lets you run away if it's too scary. Kills bugs when hunting.",
 		"price": 30,
-		"uses": 60
+		"quality": 25
 	},
 	"Flypaper": {
 		"name": "Flypaper",
 		"description": "Deadly sticky paper for flies. Kills bugs when levelling up.",
 		"price": 14,
-		"uses": -1 # lasts the whole month
+		"quality": 2 # lasts the whole month
 	},
 
 	# fertilizers
@@ -30,21 +30,21 @@ shopitems = {
 		"name": "Synthetic fertilizer",
 		"description": "Manmade stuff to make plants grow healthier. Gives extra xp when chatting.",
 		"price": 50,
-		"uses": -1
+		"quality": 1
 	},
 	"Organic fertilizer": {
 		"name": "Organice fertilizer",
 		"description": "Natural stuff to make plants grow healthier. And by natural, we mean it's poop. Gives extra xp when chatting.",
 		"price": 75,
-		"uses": -3 # lasts three months
+		"quality": 5
 	},
 
 	# long-lasting products
 	"Python": {
 		"name": "Python",
 		"description": "A mostly-friendly python to live in your tree. Kills extra bugs when hunting, and occasionally gives xp boosts when chatting.",
-		"price": 100,
-		"uses": -12 # six month lifespan wow
+		"price": 500,
+		"quality": 50
 	}
 
 }

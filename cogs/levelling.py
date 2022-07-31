8676c9e71028a81f0b00a04674b06354854b4732
Jean <x22me22x@outlook.com>
2022-07-31 13:09:54 +0300

    make the reward $$$ more realistic

--- cogs/levelling.py
+++ cogs/levelling.py
@@ -108,7 +108,7 @@ class Levelling(commands.Cog):
 
 				if data[guild][author]["xp"] > xp_needed:
 					currentlevel += 1
-					data[guild][author]["$$$"] += currentlevel * 100
+					data[guild][author]["$$$"] += currentlevel * 20
 					await message.reply(f"{message.author.mention} has leveled up to level {currentlevel}!")
 
 				db.write(data)

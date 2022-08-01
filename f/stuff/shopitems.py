a02ffd211d170e991b5f219e7ffbe23be71ce14f
Jean <x22me22x@outlook.com>
2022-08-01 11:18:36 +0300

    lowercase everything

--- f/stuff/shopitems.py
+++ f/stuff/shopitems.py
@@ -1,28 +1,28 @@
 shopitems = {
 	# bug killers
-	"Bug Spray": {
-		"name": "Bug Spray",
+	"bug spray": {
+		"name": "bug spray",
 		"description": "Spray pests (and enemies) away with this bug spray. Kills bugs when hunting.",
 		"price": 7, # you can buy it in Daiso!
 		"kill_multi": 1,
 		"equip": False,
 	},
-	"Slippers": {
-		"name": "Slippers",
+	"slippers": {
+		"name": "slippers",
 		"description": "The classic bug-slapper. Kills bugs when hunting.",
 		"price": 15, # also from Daiso!
 		"kill_multi": 5,
 		"equip": False
 	},
-	"Trainers": {
-		"name": "Trainers",
+	"trainers": {
+		"name": "trainers",
 		"description": "The bug-stomper that also lets you run away if it's too scary. Kills bugs when hunting.",
 		"price": 75,
 		"kill_multi": 25,
 		"equip": False
 	},
-	"Flypaper": {
-		"name": "Flypaper",
+	"flypaper": {
+		"name": "flypaper",
 		"description": "Deadly sticky paper for flies. Kills bugs when hunting. Lasts for the entire month.",
 		"price": 14,
 		"kill_multi": 5,
@@ -30,15 +30,15 @@ shopitems = {
 	},
 
 	# fertilizers
-	"Synthetic fertilizer": {
-		"name": "Synthetic fertilizer",
+	"synthetic fertilizer": {
+		"name": "synthetic fertilizer",
 		"description": "Manmade stuff to make plants grow healthier. Gives extra xp when chatting.",
 		"price": 150,
 		"xp_multi": 1,
 		"equip": True
 	},
-	"Organic fertilizer": {
-		"name": "Organice fertilizer",
+	"organic fertilizer": {
+		"name": "organic fertilizer",
 		"description": "Natural stuff to make plants grow healthier. And by natural, we mean it's poop. Gives extra xp when chatting.",
 		"price": 200,
 		"xp_multi": 5,
@@ -46,8 +46,8 @@ shopitems = {
 	},
 
 	# long-lasting products
-	"Python": {
-		"name": "Python",
+	"python": {
+		"name": "python",
 		"description": "A mostly-friendly python to live in your tree. Kills extra bugs when hunting, and occasionally gives xp boosts when chatting.",
 		"price": 1300,
 		"kill_multi": 50,

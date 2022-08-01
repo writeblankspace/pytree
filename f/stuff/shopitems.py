42e21985731e7afb2711113644ecd1e404313698
Jean <x22me22x@outlook.com>
2022-08-01 15:39:44 +0300

    edit it once again to fit stuff

--- f/stuff/shopitems.py
+++ f/stuff/shopitems.py
@@ -2,29 +2,33 @@ shopitems = {
 	# bug killers
 	"bug spray": {
 		"name": "bug spray",
-		"description": "Spray pests (and enemies) away with this bug spray. Kills bugs when hunting.",
+		"description": "Spray pests (and enemies) away with this bug spray.",
+		"use": "Kills 1 bug when hunting.",
 		"price": 7, # you can buy it in Daiso!
 		"kill_multi": 1,
 		"equip": False,
 	},
 	"slippers": {
 		"name": "slippers",
-		"description": "The classic bug-slapper. Kills bugs when hunting.",
+		"description": "The classic bug-slapper.",
+		"use": "Kills 3 bugs when hunting.",
 		"price": 15, # also from Daiso!
-		"kill_multi": 5,
+		"kill_multi": 3,
 		"equip": False
 	},
 	"trainers": {
 		"name": "trainers",
-		"description": "The bug-stomper that also lets you run away if it's too scary. Kills bugs when hunting.",
-		"price": 75,
+		"description": "The bug-stomper that also lets you run.",
+		"use": "Kills 25 bugs when hunting.",
+		"price": 100,
 		"kill_multi": 25,
 		"equip": False
 	},
 	"flypaper": {
 		"name": "flypaper",
-		"description": "Deadly sticky paper for flies. Kills bugs when hunting. Lasts for the entire month.",
-		"price": 14,
+		"description": "Deadly sticky paper for flies.",
+		"use": "Kills 5 bugs when hunting. Lasts for the entire month.",
+		"price": 100,
 		"kill_multi": 5,
 		"equip": True
 	},
@@ -32,14 +36,16 @@ shopitems = {
 	# fertilizers
 	"synthetic fertilizer": {
 		"name": "synthetic fertilizer",
-		"description": "Manmade stuff to make plants grow healthier. Gives extra xp when chatting.",
+		"description": "Manmade stuff to make plants grow healthier.",
+		"use": "Gives some extra xp when chatting.",
 		"price": 150,
 		"xp_multi": 1,
 		"equip": True
 	},
 	"organic fertilizer": {
 		"name": "organic fertilizer",
-		"description": "Natural stuff to make plants grow healthier. And by natural, we mean it's poop. Gives extra xp when chatting.",
+		"description": "Natural stuff (poop) to make plants grow healthier.",
+		"use": "Gives lots of extra xp when chatting.",
 		"price": 200,
 		"xp_multi": 5,
 		"equip": True
@@ -48,9 +54,10 @@ shopitems = {
 	# long-lasting products
 	"python": {
 		"name": "python",
-		"description": "A mostly-friendly python to live in your tree. Kills extra bugs when hunting, and occasionally gives xp boosts when chatting.",
+		"description": "A mostly-friendly python to live in your tree.",
+		"use": "Kills 25 extra bugs when hunting, and occasionally gives xp boosts when chatting. This is a lifetime product.",
 		"price": 1300,
-		"kill_multi": 50,
+		"kill_multi": 25,
 		"xp_multi": 13,
 		"equip": True
 	}

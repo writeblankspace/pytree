1e3d63ff298a1f0718e44250c2a971802a5a7da6
Jean <x22me22x@outlook.com>
2022-08-06 19:27:15 +0300

    add different bug types too

--- cogs/hunting.py
+++ cogs/hunting.py
@@ -7,12 +7,14 @@ from db.db import db
 import asyncio
 from f.__index__ import *
 
+
 class Hunting(commands.Cog):
 	def __init__(self, bot: commands.Bot) -> None:
 		self.bot = bot
 		self.currency = "⚇"
-	
-	group = app_commands.Group(name="bugs", description="Bug-hunting commands: hunt bugs to earm xp and money.")
+
+	group = app_commands.Group(
+		name="bugs", description="Bug-hunting commands: hunt bugs to earm xp and money.")
 
 	class BugHunt(discord.ui.View):
 		def __init__(self, init_user: discord.User, multi: int, embed: discord.Embed):
@@ -25,7 +27,7 @@ class Hunting(commands.Cog):
 			self.bugs_killed = 0
 
 			super().__init__()
-		
+
 		def use_item(self, interaction: discord.Interaction, itemname: str):
 			item = shopitems[itemname]
 			kill_multi = item["kill_multi"]
@@ -41,7 +43,7 @@ class Hunting(commands.Cog):
 				def __init__(self, success, embed):
 					self.success = success
 					self.embed = embed
-			
+
 			inventory = data[guildid][userid]["inventory"]
 
 			if itemname in inventory:
@@ -58,7 +60,8 @@ class Hunting(commands.Cog):
 				description = self.embed.description
 				# find everything between the "```"s
 				logs = description.split("```")[1]
-				newlogs = logs + "\n" + f"{user.name}:: used {itemname} [+{kill_multi}]"
+				newlogs = logs + "\n" + \
+					f"{user.name}:: used {itemname} [+{kill_multi}]"
 				description = description.replace(logs, newlogs)
 
 				self.embed.__setattr__("description", description)
@@ -83,7 +86,7 @@ class Hunting(commands.Cog):
 				await interaction.response.edit_message(embed=results.embed)
 			else:
 				await interaction.response.send_message(embed=results.embed, ephemeral=True)
-		
+
 		@discord.ui.button(label='slippers', style=discord.ButtonStyle.secondary, custom_id='slippers')
 		async def slippers(self, interaction: discord.Interaction, button: discord.ui.Button):
 			results = self.use_item(interaction, "slippers")
@@ -101,7 +104,7 @@ class Hunting(commands.Cog):
 				await interaction.response.edit_message(embed=results.embed)
 			else:
 				await interaction.response.send_message(embed=results.embed, ephemeral=True)
-		
+
 		@discord.ui.button(label='flypaper', style=discord.ButtonStyle.secondary, custom_id='flypaper')
 		async def flypaper(self, interaction: discord.Interaction, button: discord.ui.Button):
 			results = self.use_item(interaction, "flypaper")
@@ -110,13 +113,13 @@ class Hunting(commands.Cog):
 				await interaction.response.edit_message(embed=results.embed)
 			else:
 				await interaction.response.send_message(embed=results.embed, ephemeral=True)
-		
+
 	@group.command(name="hunt")
 	async def hunt(self, interaction: discord.Interaction) -> None:
 		"""
 		Hunt for bugs to earn xp and money."""
 		# await interaction.response.defer(ephemeral=True)
-		
+
 		user = interaction.user
 		guildid = str(interaction.guild.id)
 		userid = str(user.id)
@@ -131,8 +134,9 @@ class Hunting(commands.Cog):
 		kill_multi = multi.kill_multi
 
 		r = random.uniform
-		
-		bug_amount = [r(0.1, 2), r(0.1, 2), r(0.1, 2), r(2, 5), r(2, 5), r(1, 10)]
+
+		bug_amount = [r(0.1, 2), r(0.1, 2), r(
+			0.1, 2), r(2, 5), r(2, 5), r(1, 10)]
 		# pick random from bug_amount
 		bugs = random.choice(bug_amount) * kill_multi
 		bugs = int(bugs)
@@ -141,14 +145,14 @@ class Hunting(commands.Cog):
 			bugs = 1
 
 		embed = discord.Embed(
-			title = f"{user.name} started a bug hunt!",
-			description = f"There are **{bugs} bugs** in the area.",
-			color = templates.colours["draw"]
+			title=f"{user.name} started a bug hunt!",
+			description=f"There are **{bugs} bugs** in the area.",
+			color=templates.colours["draw"]
 		)
 
 		footertext = f"This hunt starts in 3 seconds."
 
-		embed.set_footer(text = footertext)
+		embed.set_footer(text=footertext)
 
 		await interaction.response.send_message(embed=embed)
 
@@ -157,16 +161,16 @@ class Hunting(commands.Cog):
 		await asyncio.sleep(3)
 
 		embed = discord.Embed(
-			title = f"{user.name} is hunting for bugs!",
-			description = f"There are **{bugs} bugs** in the area. Click on the items you want to use hunt the bugs. There is a 1/3 chance of one bug being killed per attempt.\n" + \
-				"Other users can join in too! Rewards will be shared equally.\n" + \
-				"```asciidoc\n" + \
-				f"{user.name}:: started the bug hunt```",
-			color = templates.colours["success"]
+			title=f"{user.name} is hunting for bugs!",
+			description=f"There are **{bugs} bugs** in the area. Click on the items you want to use hunt the bugs. There is a 1/3 chance of one bug being killed per attempt.\n" +
+			"Other users can join in too! Rewards will be shared equally.\n" +
+			"```asciidoc\n" +
+			f"{user.name}:: started the bug hunt```",
+			color=templates.colours["success"]
 		)
 
 		embed.set_footer(
-			text = f"This hunt ends in 15 seconds."
+			text=f"This hunt ends in 15 seconds."
 		)
 
 		view = self.BugHunt(user, kill_multi, embed)
@@ -176,10 +180,10 @@ class Hunting(commands.Cog):
 		await asyncio.sleep(15)
 
 		embed = discord.Embed(
-			title = f"{user.name}'s hunt ended!",
-			description = f"There were **{bugs} bugs** in the area." + \
-				"\nPlease wait while the results are calculated.",
-			color = templates.colours["draw"]
+			title=f"{user.name}'s hunt ended!",
+			description=f"There were **{bugs} bugs** in the area." +
+			"\nPlease wait while the results are calculated.",
+			color=templates.colours["draw"]
 		)
 
 		data = db.read()
@@ -196,14 +200,65 @@ class Hunting(commands.Cog):
 		for i in range(attempts):
 			if random.randint(1, 3) == 1:
 				successes += 1
-		
+
 		# no more than {bugs} that can be killed (duh)
 		if successes > bugs:
 			successes = bugs
 
-		money_reward = int(successes / len(users)) * kill_multi
-		xp_reward = int((successes / len(users)) / 2) * kill_multi
+		# different bug types return different rewards!
+
+		# 🐝🐞🐜🪰🦟🪳🪲🕷️
+		# :bee::lady_beetle::ant::fly::mosquito::cockroach::beetle::spider:
+		bug_types = {
+			"bee": {"emoji": "🐝", "prize": 10, "plural": "bees"},
+			"lady beetle": {"emoji": "🐞", "prize": 7, "plural": "lady beetles"},
+			"ant": {"emoji": "🐜", "prize": 1}, "plural": "ants",
+			"fly": {"emoji": "🪰", "prize": 1, "plural": "flies"},
+			"mosquito": {"emoji": "🦟", "prize": 2, "plural": "mosquitoes"},
+			"cockroach": {"emoji": "🪳", "prize": 5, "plural": "cockroaches"},
+			"beetle": {"emoji": "🪲", "prize": 6, "plural": "beetles"},
+			"spider": {"emoji": "🕷️", "prize": 10, "plural": "spiders"}
+		}
+		bug_types_rarity = [
+			"ant", "ant", "ant",
+			"fly", "fly", "fly", "fly", "fly", "fly", "fly", "fly", "fly", "fly", 
+			"cockroach", "cockroach", "cockroach", "cockroach", "cockroach", 
+			"mosquito", "mosquito", "mosquito",
+			"bee", "bee",
+			"lady_beetle", "beetle", "beetle", "spider"
+		]
+
+		bug_types_caught = []
+		for i in range(successes):
+			bug_type = random.choice(bug_types_rarity)
+			bug_types_caught.append(bug_type)
+
+		# return a list of all bug types in the bug_types_caught list
+		bug_types_caught = list(set(bug_types_caught))
+		bug_types_caught_str = []
+
+		for bug_type in bug_types_caught:
+			count = bug_types_caught.count(bug_type)
+			emoji = bug_types[bug_type]["emoji"]
+			bug_type = bug_types[bug_type]["plural"]
+			reward = bug_types[bug_type]["prize"] * count
+			bug_types_caught_str.append(f"{emoji} {bug_type}:: {count} [+{reward} {self.currency}]")
+
+		bug_types_caught_str = "\n".join(bug_types_caught_str)
+
+		if bug_types_caught_str == "":
+			bug_types_caught_str = "[None]"
 		
+		bug_rewards = 0
+
+		# get the rewards for each bug
+		for bug_type in bug_types_caught:
+			bug_rewards += bug_types[bug_type]["prize"]
+
+
+		money_reward = int(bug_rewards / len(users)) * kill_multi
+		xp_reward = int((bug_rewards / len(users)) / 2) * kill_multi
+
 		usernames = []
 
 		for auserid in users:
@@ -214,22 +269,31 @@ class Hunting(commands.Cog):
 			# get the user as discord.User
 			user = self.bot.get_user(int(auserid))
 			usernames.append(user.name)
-		
+
 		db.write(data)
 
 		embed = discord.Embed(
-			title = f"{user.name}'s hunt results:",
-			description = f"**{successes} bugs** were killed out of {attempts} attempts." + "\n" + \
-				f"There were {bugs} bugs in the area." + "\n" + \
-				f"Each user earned **{money_reward} {self.currency}** and **{xp_reward} xp.**" + "\n" + \
-				f"```{', '.join(usernames)}```",
-			color = templates.colours["success"]
+			title=f"{user.name}'s hunt results:",
+			description=f"**{successes} bugs** were killed out of {attempts} attempts." + "\n" +
+			f"There were {bugs} bugs in the area." + "\n" +
+			f"Each user earned **{money_reward} {self.currency}** and **{xp_reward} xp.**",
+			color=templates.colours["success"]
+		)
+
+		embed.add_field(
+			name = "Bugs caught",
+			value = "```asciidoc\n" + bug_types_caught_str + "```",
+			inline = True
+		)
+
+		embed.add_field(
+			name = "Participants",
+			value = f"```{', '.join(usernames)}```",
+			inline = True
 		)
 
 		await interaction.edit_original_message(view=None, embed=embed)
 
 
-
-
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Hunting(bot))
+	await bot.add_cog(Hunting(bot))

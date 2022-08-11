27e2bf07c758bb28bab9010f898c08bfb897b199
Jean <x22me22x@outlook.com>
2022-08-11 15:35:07 +0300

    thicker loader

--- f/templates.py
+++ f/templates.py
@@ -1,7 +1,7 @@
 import discord
 class Theme(object):
 	def __init__(self):
-		self.loader = "<a:loader:1007256284178939904>"
+		self.loader = "<a:loader:1007265720926744586>"
 		self.colours = self.Colours()
 		self.colors = self.Colours()
 	class Colours(object):
